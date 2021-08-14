44af88941a49f7cc2b287699743f81cf024e0da1
James Roberts <jamesroberts.dev@gmail.com>
2021-08-14 19:35:14 +0200

    Small clean up

---
+++ fast-wsgi/request.c
@@ -0,0 +1,68 @@
+#include "server.h"
+#include "request.h"
+#include "llhttp.h"
+
+static void set_header(PyObject* headers, PyObject* key, const char* value, size_t length) {
+    printf("setting header\n");
+    PyObject* item = PyUnicode_FromStringAndSize(value, length);
+    PyDict_SetItem(headers, key, item);
+    Py_DECREF(item);
+}
+
+int on_message_begin(llhttp_t* parser) {
+    printf("on message begin\n");
+    Request* request = (Request*)parser->data;
+    request->headers = PyDict_New();
+    return 0;
+};
+
+int on_url(llhttp_t* parser, const char* url, size_t length) {
+    printf("on url\n");
+    Request* request = (Request*)parser->data;
+    PyObject* header = Py_BuildValue("s", "url");
+
+    Py_INCREF(header);
+    set_header(request->headers, header, url, length);
+    return 0;
+};
+
+int on_body(llhttp_t* parser, const char* body, size_t length) {
+    printf("on body\n");
+    Request* request = (Request*)parser->data;
+    PyObject* header = Py_BuildValue("s", "body");
+    Py_INCREF(header);
+    set_header(request->headers, header, body, length);
+    return 0;
+};
+
+PyObject* current_header;
+
+int on_header_field(llhttp_t* parser, const char* header, size_t length) {
+    printf("on header field\n");
+    Request* request = (Request*)parser->data;
+    current_header = PyUnicode_FromStringAndSize(header, length);
+    Py_INCREF(current_header);
+    return 0;
+};
+
+int on_header_value(llhttp_t* parser, const char* value, size_t length) {
+    printf("on header value\n");
+    Request* request = (Request*)parser->data;
+    set_header(request->headers, current_header, value, length);
+    return 0;
+};
+
+int on_message_complete(llhttp_t* parser) {
+    printf("on message complete\n");
+    return 0;
+};
+
+void configure_parser_settings() {
+    parser_settings.on_url = on_url;
+    parser_settings.on_body = on_body;
+    parser_settings.on_header_field = on_header_field;
+    parser_settings.on_header_value = on_header_value;
+    parser_settings.on_message_begin = on_message_begin;
+    parser_settings.on_message_complete = on_message_complete;
+}
+
--- fast-wsgi/server.c
+++ fast-wsgi/server.c
@@ -5,7 +5,6 @@
 #include "llhttp.h"
 
 #include "server.h"
-
 #include "request.h"
 
 #define SIMPLE_RESPONSE \
@@ -16,69 +15,12 @@
   "Hello, World!\n"
 
 static const char* HOST = "0.0.0.0";
-static const int PORT = 5000;
+static const int PORT = 5001;
 static const int BACKLOG = 256;
 
 PyObject* app;
 
 static uv_tcp_t server;
-
-static void set_header(PyObject* headers, PyObject* key, const char* value, size_t length) {
-    printf("setting header: %s\n", value);
-    PyObject* item = PyUnicode_FromStringAndSize(value, length);
-    PyDict_SetItem(headers, key, item);
-    Py_DECREF(item);
-}
-
-int on_message_begin(llhttp_t* parser) {
-    printf("on message begin\n");
-    Request* request = (Request*)parser->data;
-    request->headers = PyDict_New();
-    return 0;
-};
-
-int on_url(llhttp_t* parser, const char* url, size_t length) {
-    printf("on url\n");
-    Request* request = (Request*)parser->data;
-    PyObject* header = Py_BuildValue("s", "url");
-
-    Py_INCREF(header);
-    set_header(request->headers, header, url, length);
-    return 0;
-};
-
-int on_body(llhttp_t* parser, const char* body, size_t length) {
-    printf("on body\n");
-    Request* request = (Request*)parser->data;
-    PyObject* header = Py_BuildValue("s", "body");
-    Py_INCREF(header);
-    set_header(request->headers, header, body, length);
-    return 0;
-};
-
-PyObject* current_header;
-
-int on_header_field(llhttp_t* parser, const char* header, size_t length) {
-    printf("on header field\n");
-    Request* request = (Request*)parser->data;
-    current_header = PyUnicode_FromStringAndSize(header, length);
-    Py_INCREF(current_header);
-    return 0;
-};
-
-int on_header_value(llhttp_t* parser, const char* value, size_t length) {
-    printf("on header value\n");
-    Request* request = (Request*)parser->data;
-    set_header(request->headers, current_header, value, length);
-    return 0;
-};
-
-int on_message_complete(llhttp_t* parser) {
-    printf("on message complete\n");
-    return 0;
-};
-
-static llhttp_settings_t parser_settings;
 
 static uv_buf_t response_buf;
 
@@ -148,15 +90,6 @@ void connection_cb(struct uv_stream_s* handle, int status) {
     }
 }
 
-void configure_parser_settings() {
-    parser_settings.on_url = on_url;
-    parser_settings.on_body = on_body;
-    parser_settings.on_header_field = on_header_field;
-    parser_settings.on_header_value = on_header_value;
-    parser_settings.on_message_begin = on_message_begin;
-    parser_settings.on_message_complete = on_message_complete;
-}
-
 int main() {
     loop = uv_default_loop();
 
@@ -185,7 +118,7 @@ int main() {
 
 PyObject* run_server(PyObject* self, PyObject* args) {
     PyArg_UnpackTuple(args, "ref", 1, 1, &app);
-    PyObject* retval = PyObject_CallFunctionObjArgs(
+    PyObject_CallFunctionObjArgs(
         app, Py_BuildValue("s", "======> String from C!"), NULL
     );
     // main();
