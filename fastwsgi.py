defda60f14fbb1b38b4768f5050893ac57a08b87
remittor <remittor@gmail.com>
2022-10-27 21:58:06 +0300

    Fix loglevel setting for some cases

--- fastwsgi.py
+++ fastwsgi.py
@@ -6,11 +6,21 @@ import click
 import _fastwsgi
 from pkg_resources import get_distribution
 
+LL_DISABLED    = 0
+LL_FATAL_ERROR = 1
+LL_CRIT_ERROR  = 2
+LL_ERROR       = 3
+LL_WARNING     = 4
+LL_NOTICE      = 5
+LL_INFO        = 6
+LL_DEBUG       = 7
+LL_TRACE       = 8
+
 NUM_WORKERS = 4
 HOST = "0.0.0.0"
 PORT = 5000
 BACKLOG = 1024
-LOGLEVEL = 3
+LOGLEVEL = LL_ERROR
 
 
 def run_multi_process_server(app):
@@ -22,7 +32,7 @@ def run_multi_process_server(app):
             print(f"Worker process added with PID: {pid}")
         else:
             try:
-                _fastwsgi.run_server(app, HOST, PORT, BACKLOG, 0)
+                _fastwsgi.run_server(app, HOST, PORT, BACKLOG, LOGLEVEL)
             except KeyboardInterrupt:
                 exit()
 
@@ -60,13 +70,13 @@ def print_server_details(host, port):
 @click.version_option(version=get_distribution("fastwsgi").version, message="%(version)s")
 @click.option("--host", help="Host the socket is bound to.", type=str, default=HOST, show_default=True)
 @click.option("-p", "--port", help="Port the socket is bound to.", type=int, default=PORT, show_default=True)
-@click.option("-l", "--logging", help="Logging level.", type=int, default=LOGLEVEL, show_default=True)
+@click.option("-l", "--loglevel", help="Logging level.", type=int, default=LOGLEVEL, show_default=True)
 @click.argument(
     "wsgi_app_import_string",
     type=str,
     required=True,
 )
-def run_from_cli(host, port, wsgi_app_import_string, logging):
+def run_from_cli(host, port, wsgi_app_import_string, loglevel):
     """
     Run FastWSGI server from CLI
     """
@@ -78,7 +88,7 @@ def run_from_cli(host, port, wsgi_app_import_string, logging):
 
     print_server_details(host, port)
     print(f"Server listening at http://{host}:{port}")
-    _fastwsgi.run_server(wsgi_app, host, port, BACKLOG, logging)
+    _fastwsgi.run_server(wsgi_app, host, port, BACKLOG, loglevel)
 
 
 def run(wsgi_app, host=HOST, port=PORT, backlog=1024, loglevel=LOGLEVEL):
