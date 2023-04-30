38b89bc0669d4105b41f0e3b1625876e177c7a56
remittor <remittor@gmail.com>
2023-04-30 09:26:22 +0300

    Forced use clang compiler, if installed
    This allows for a slight speed increase over the GCC compiler.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import os
+import sys
 import glob
+import subprocess
+import platform
+import re
 from setuptools import setup
 from distutils.core import Extension
 from distutils.command.build_ext import build_ext
@@ -13,6 +17,37 @@ module = Extension(
     include_dirs=["llhttp/include", "libuv/include"],
 )
 
+def get_compiler_version(exe_name):
+    version = None
+    ver_major = None
+    ver_minor = None
+    try:
+        res = subprocess.run([exe_name, '--version'], stdout=subprocess.PIPE)
+        if res.returncode == 0:
+            version = res.stdout.decode('utf8')
+            ver_s = re.search(r"[\D+](\d+)[.](\d+)[\D+]", version)
+            if ver_s:
+                ver_major = int(ver_s.group(1))
+                ver_minor = int(ver_s.group(2))
+    except:
+        pass
+    return version, ver_major, ver_minor
+
+
+current_compiler = os.getenv('CC', "")
+
+# Forced use clang compiler, if installed
+if platform.system() == "Linux" and current_compiler == "":
+    for cc_ver in range(40, 7, -1):
+        cc = 'clang-%d' % cc_ver
+        text, ver_major, ver_minor = get_compiler_version(cc)
+        if text and ver_major:
+            current_compiler = cc
+            print('Change current compiler to {}'.format(cc))
+            os.environ['CC'] = cc
+            break
+
+
 class build_all(build_ext):
     def initialize_options(self):
         build_ext.initialize_options(self)
@@ -22,17 +57,39 @@ class build_all(build_ext):
 
         setup_libuv.build_libuv(self)
 
-        compiler = self.compiler.compiler_type
-        print("Current compiler:", compiler)
+        compiler_type = self.compiler.compiler_type
+        print("Current compiler type:", compiler_type)
+        try:
+            compiler_cmd = self.compiler.compiler
+            print('Current compiler: {}'.format(compiler_cmd))
+        except:
+            compiler_cmd = None
+        
+        compiler = ""
+        if compiler_cmd:
+            compiler = compiler_cmd[0]
+        if compiler_type == "msvc":
+            compiler = "msvc"
+        
+        compiler_ver_major = 0
+        if compiler_type == 'unix' and compiler:
+            version, c_ver_major, c_ver_minor = get_compiler_version(compiler)
+            print('Current compiler version: {}.{}'.format(c_ver_major, c_ver_minor))
+            if c_ver_major:
+                compiler_ver_major = c_ver_major
 
         for ext in self.extensions:
             if ext == module:
-                if compiler == 'msvc':
+                if compiler_type == 'msvc':
                     ext.extra_compile_args = [ '/Oi', '/Oy-', '/W3', '/WX-', '/Gd', '/GS' ]
                     ext.extra_compile_args += [ '/Zc:forScope', '/Zc:inline', '/fp:precise', '/analyze-' ]
                 else:
                     ext.extra_compile_args = [ "-O3", "-fno-strict-aliasing", "-fcommon", "-g", "-Wall" ]
                     ext.extra_compile_args += [ "-Wno-unused-function", "-Wno-unused-variable" ]
+                    if compiler.startswith("gcc") and compiler_ver_major >= 8:
+                        ext.extra_compile_args += [ "-Wno-unused-but-set-variable" ]
+                    if compiler.startswith("clang") and compiler_ver_major >= 13:
+                        ext.extra_compile_args += [ "-Wno-unused-but-set-variable" ]
         
         build_ext.build_extensions(self)
 
